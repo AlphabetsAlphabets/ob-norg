51ae2b12200bca701ca8b51f91b72b0216f384f5
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-07-30 18:08:47 +0800

    fix(filename): Includes # or | now it doesn't

--- main.lua
+++ main.lua
@@ -6,50 +6,29 @@
 -- [[Note | text]] -> {:Note:}[text]
 -- [[Note#Heading | text]] -> {:Note:*Heading}[text]
 
+local function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
 local strings = {
-  "[[Note]]",
   "[[Note#^7ef311]]",
   "[[Note#Heading]]",
-  "[[Note | text]]",
-  "[[Note#Heading | text]]",
-  "[[Note#Heading|text]]",
-  "[[Note#Heading| text]]",
+  "[[Note | Text]]",
+  "[[Note]]",
 }
 
 for _, string in ipairs(strings) do
-  print(string.format("Link to format: '%s'", string))
   local pattern = "%[%[(.-)%]%]"
-  local link = string.match(string, pattern)
+  local link = string:match(pattern)
+  print(string.format("Link to format: '%s'", link))
 
-  local linkToHeading = string.find(link, "#", 1, true)
-  local customText = string.find(link, "|", 1, true)
+  local linkToHeading = link:find("#", 1, true)
+  linkToHeading = linkToHeading and linkToHeading - 1 or link:len()
 
-  local filename = nil
-  filename = string.sub(link, 0, (linkToHeading and linkToHeading - 1) or customText)
-  if filename == nil then
-    filename = link
-  end
+  local textBarrier = link:find("#", 1, true)
+  textBarrier = textBarrier and textBarrier - 1 or link:len()
 
-  if customText ~= nil then
-    local leftIsSpace = string.sub(filename, customText - 1, customText - 1) == ' '
-    local rightIsSpace = string.sub(filename, customText + 1) == ' '
-
-    if leftIsSpace and rightIsSpace == false then
-       filename = string.sub(filename, 0, customText - 2)
-    end
-
-  end
-
-  local new = "{:" .. filename .. ":}"
-
-  -- check for "#"
-  local headingIndex = string.find(link, "#", 1, true)
-  if headingIndex ~= nil then
-    local heading = string.sub(link, headingIndex)
-    -- Do a check for ^ to the right if there is a ^ to the right
-    -- I can safely discard it and exit out of this if statement.
-    print(string.format("Heading: '%s'", heading))
-  end
-
-  print(string.format("Result: '%s'\n", new))
+  local stop = (linkToHeading or textBarrier)
+  local filename = link:sub(0, stop)
+  print(filename)
 end
